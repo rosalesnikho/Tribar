bb86128ba84cfec6858217f465d132bb6f0621cd
rosalesnikho <rosales.nikho@gmail.com>
2019-09-09 15:06:55 -0500

    added logic for chain validation

--- Blockchain.js
+++ Blockchain.js
@@ -1,4 +1,5 @@
 const Block = require('./Block');
+const cryptoHash = require('./crypto-hash');
 
 class Blockchain {
     constructor () {
@@ -11,6 +12,25 @@ class Blockchain {
             data
         });
         this.chain.push(newDataBlock);
+    }
+
+    static isValidChain(chain) {
+        if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
+            return false;
+        }
+        for(let i = 1; i < chain.length; i++ ) {
+            const block = chain[i];
+            const correctLastHash = chain[i-1].hash;
+            const { timestamp, lastHash, hash, data } = block;
+
+            if( lastHash !== correctLastHash) return false;
+
+            const validatedHash = cryptoHash(timestamp, lastHash, data);
+
+            if(hash !== validatedHash) return false;
+         }
+
+        return true;
     }
 }
 
