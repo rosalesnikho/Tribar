95b0b6ec3b25f47beb7f942cd72d27e51851680f
nikvicros <nikho.rosales@gmail.com>
2019-09-14 18:15:39 -0500

    added express setup

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+const bodyParses = require('body-parser');
 const express = require ('express');
 const Blockchain = require('./Blockchain');
 
+// Instantiate Express application
 const app  = express();
 const blockChain = new Blockchain();
 
+// Uses body parses to receive and send JSON
+app.use(bodyParses.json());
+
+
+// Get request to read block chain data
 app.get('/api/blocks', (req, res ) => {
 	res.json(blockChain.chain)
-
-
 });
-const port = 3000;
 
+
+// Post Request to send new instances of a block
+app.post('/api/mine', (req, res) => {
+	 const {data} = req.body;
+	 blockChain.addBlock({ data });
+
+	 // Once block chain is added redirects to /api/blocks end point
+	 res.redirect('/api/blocks');
+});
+
+
+// Port Setup
+const port = 3000;
 app.listen(port, () => console.log('Listening at port: ' + port));
 
