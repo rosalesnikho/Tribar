35f74db80bf9bc5ae334d57ca66f741e76aa5bc0
nikvicros <nikho.rosales@gmail.com>
2019-10-05 02:23:43 -0500

    added depedencies and build commands

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import { render } from 'react-dom';
 
 render(
-	<div>Tribar from React</div>,
-	document.getElementById('root')
+	<div>Tribar using Parcel! Now!56</div>, document.getElementById('root')
 );
