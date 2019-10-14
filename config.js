c79a30672fd12172e3ff6713ba7af61d45dcaaff
Nikho Rosales <nikhorosales@nikhos-air.attlocal.net>
2019-10-14 11:37:27 -0500

    added block js components

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ const INITIAL_DIFFICULTY = 3;
 */
 const GENESIS_DATA = {
 	timestamp: 1,
-	lastHash: 'gen-last---',
-	hash: 'gen-hash---',
+	lastHash: 'GENESIS---last---',
+	hash: 'GENESIS---hash---',
 	difficulty: INITIAL_DIFFICULTY,
 	nonce: 0,
 	data: []
