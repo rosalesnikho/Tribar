43040ce5c50ee5d2058953245f7dd4f4a312b7d5
Nikho Rosales <nikhorosales@nikhos-air.attlocal.net>
2019-10-14 14:36:45 -0500

    added toggle buttons

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 
 class Block extends Component {
-    render() {
-        const { timestamp, hash, data } = this.props.block;
-        // Hash render & stringify
-        const hashDisplay = `${hash.substring(0, 15)}...`;
+    // Set initial state of the Block component
+    state = { displayTransaction: false };
+
+    // Toggles the showing of more data
+    toggleTransaction = () => {
+        this.setState({ displayTransaction: !this.setState.displayTransaction })
+    }
+
+    // Getter for displaying transactional data from the block
+    get displayTransaction() {
+        const { data } = this.props.block;
 
         // Data render & stringify
         // Checks length is > 15 characters
@@ -12,11 +19,37 @@ class Block extends Component {
         const dataRender = stringData.length > 35 ?
             `${stringData.substring(0, 35)}...` : stringData;
 
+        if (this.state.displayTransaction) {
+            return (
+                <div>
+                    {JSON.stringify(data)}
+                    <br />
+                    <button className="button-primary" onClick={this.toggleTransaction}>View Less</button>
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                <div>Data: {dataRender}</div>
+                <button className="button-primary" onClick={this.toggleTransaction}>View More</button>
+            </div>
+        );
+    }
+
+    render() {
+        const { timestamp, hash } = this.props.block;
+
+        // Hash render & stringify
+        const hashDisplay = `${hash.substring(0, 15)}...`;
+
+
+
         return (
             <div className='Block'>
                 <div>Hash: {hashDisplay}</div>
                 <div>Timestamp: { new Date (timestamp).toLocaleString()}</div>
-                <div>Data: {dataRender}</div>
+                {this.displayTransaction}
             </div>
         );
     }
