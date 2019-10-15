52b48860fc8e1e8824de70e1f4128d140e153db6
Nikho Rosales <nikhorosales@Nikhos-MacBook-Air.local>
2019-10-15 16:06:43 -0500

    added front end comps and resources, as well struture for react

---
+++ client/src/components/common/footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return(
+        <div>
+            <footer>
+                <div className="row">
+                    <div className="columns four">1</div>
+                    <div className="columns four">2</div>
+                    <div className="columns four">3</div>
+                </div>
+            </footer>
+        </div>
+    )
+};
+
+export default Footer
