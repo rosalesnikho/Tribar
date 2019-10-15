04904098fc1a2f7a097f602fdc530e96e451a8f9
Nikho Rosales <nikhorosales@Nikhos-MacBook-Air.local>
2019-10-15 10:07:39 -0500

    added blocks and data in different components

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import history from './history';
 import App from './components/App';
+import Blocks from './components/Blocks';
 
 render(
-	<App />,
+	<Router history={history}>
+		<Switch>
+			<Route exact path='/' component={App} />
+			<Route path='/blocks' component={Blocks} />
+		</Switch>
+	</Router>,
 	document.getElementById('root')
 );
